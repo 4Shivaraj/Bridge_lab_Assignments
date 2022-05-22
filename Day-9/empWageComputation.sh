b33d3c2fb9e251a7e0fb95207ee444b2303c0cd6
4Shivaraj <www.4shivaraj.gowda@gmail.com>
2022-05-22 22:14:17 +0530

    [Shivaraj]: Added daily wage calculation

--- Day-9/empWageComputation.sh
+++ Day-9/empWageComputation.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
 IS_PRESENT=1
 IS_ABSENT=0
 randomValue=0
 presentAbsent=-1
-
+TOTAL_WORKING_HOUR=8
+WAGE_PER_HOUR_FULL_DAY=20
+totalWage=0
 
 echo "Welcome to emp wage"
 
@@ -14,6 +16,9 @@ presentAbsent=$(($randomValue%2))
 if [ $presentAbsent -eq $IS_PRESENT ]
 then
         echo "Emp present"
+        totalWage=$(($TOTAL_WORKING_HOUR*$WAGE_PER_HOUR_FULL_DAY))
 else
         echo "Emp absent"
 fi
+
+echo $totalWage
